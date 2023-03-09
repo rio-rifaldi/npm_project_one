c448da14551d862c20ec0057c667774de0e75c5a
rio-rifaldi <riorifaldi2004@gmail.com>
2023-03-09 15:22:43 +0700

    add export module

--- lib/one.d.ts
+++ lib/one.d.ts
@@ -1,2 +1,2 @@
-declare const multiply: (a: number, b: number) => number;
-declare const multiplyAll: (number: number[]) => number;
+export declare const multiply: (a: number, b: number) => number;
+export declare const multiplyAll: (number: number[]) => number;
--- src/one.ts
+++ src/one.ts
@@ -1,7 +1,7 @@
-const multiply = (a: number, b: number): number => {
+export const multiply = (a: number, b: number): number => {
   return a * b;
 };
 
-const multiplyAll = (number: number[]): number => {
+export const multiplyAll = (number: number[]): number => {
   return number.reduce((acc, cur) => acc * cur);
 };
